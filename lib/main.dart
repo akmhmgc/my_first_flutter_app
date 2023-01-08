00954004693d916769a9e4dd0a1208a306eccada
akmhmgc <akimuhamaguchi@icloud.com>
2023-01-08 14:48:12 +0900

    ボタンのクリックで文字を変更する

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  final title = 'サンプルタイトル';
-  final message = 'サンプル・メッセージ';
+  final title = 'Flutterサンプル';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       home: MyHomePage(
-          title: title,
-          message: message
+        title:this.title,
       ),
     );
   }
 }
 
-//StatefulWidgetの定義
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({required this.title}): super();
   final String title;
-  final String message;
-  const MyHomePage({
-    Key? key,
-    required this.title,
-    required this.message
-  }): super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-//ステートクラス
 class _MyHomePageState extends State<MyHomePage> {
+  String _message = 'Hello!';
+
+  void _setMessage() {
+    setState(() {
+      _message = 'タップしました！';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    //以下をステートの操作に応じてへ変化するように処理をする
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Text(
-        widget.message,
-        style: TextStyle(fontSize:32.0),
+        _message,
+        style: TextStyle(fontSize: 32.0),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _setMessage,
+        tooltip: 'set message.',
+        child: Icon(Icons.star),
       ),
     );
   }
