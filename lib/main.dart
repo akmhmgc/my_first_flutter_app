8aace857f2ff8da717a97ebc794be45d187c036c
akmhmgc <akimuhamaguchi@icloud.com>
2023-04-09 13:25:02 +0900

    Hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: Text(
-        'Hello, Flutter World!!',
-        style: TextStyle(fontSize:32.0),
+  runApp(
+    const Center(
+      child: Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
       ),
-    );
-  }
+    ),
+  );
 }
