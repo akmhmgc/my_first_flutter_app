74483652c977d30249122ae5d8f72e920ce5e240
akmhmgc <akimuhamaguchi@icloud.com>
2023-08-06 18:30:51 +0900

    feat: お気に入り一覧をアップデート

--- lib/main.dart
+++ lib/main.dart
@@ -182,14 +182,26 @@ class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    var favorites = appState.favorites;
-    return ListView.builder(
-      itemCount: favorites.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(favorites[index].asLowerCase),
-        );
-      },
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
     );
   }
 }
